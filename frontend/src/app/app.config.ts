5062d9a21aea7a7e92e47e2195289f2376836b6d
aurelien-aubry <35953576+Harzak@users.noreply.github.com>
2026-03-08 15:02:42 +0100

    move primeng preset from app.config.ts to token overrides in theme.scss

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,43 +3,9 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import { definePreset } from '@primeuix/themes';
 import { routes } from './app.routes';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
 import Aura from '@primeuix/themes/aura';
-
-
-const MyPreset = definePreset(Aura, {
-  components: {
-    select: {
-      root: {
-        background: 'var(--color-dark-black)',
-        borderColor: 'var(--color-dark-grey)',
-        color: 'var(--color-light-white)',
-        hoverBorderColor: 'var(--color-light-white)',
-        focusBorderColor: 'var(--color-light-white)',
-      },
-      option: {
-        color: 'var(--color-light-white)',
-        selectedColor: 'var(--color-light-white)',
-        selectedFocusBackground: 'var(--color-lighter-green)',
-        focusColor: 'var(--color-lighter-green)',
-      },
-      dropdown:{
-        color: 'var(--color-light-white)',
-      }
-    },
-    inputtext:{
-      root: {
-        background: 'var(--color-dark-black)',
-        color: 'var(--color-light-white)',
-        borderColor: 'var(--color-dark-grey)',
-        focusBorderColor: 'var(--color-lighter-green)',
-        placeholderColor: 'var(--color-light-grey)',
-    },
-  }
-  }
-});
 
 
 export const appConfig: ApplicationConfig = {
@@ -48,6 +14,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withInterceptors([errorInterceptor])),
     provideAnimationsAsync(),
-    providePrimeNG({ theme: { preset: MyPreset } }),
+    providePrimeNG({ theme: { preset: Aura } }),
   ],
 };
